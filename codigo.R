16cf6f6bc3930b1423d0fd454c1ed19ff6b94c36
unknown <juandsep@gmail.com>
2020-11-07 22:52:42 -0500

    exploracion de datos

--- codigo.R
+++ codigo.R
@@ -14,7 +14,6 @@
 # Intro ####
 
 # ambiente
-
 rm(list = ls())
 gc()
 options(scipen = '999')
@@ -32,14 +31,52 @@ pacman::p_load(
   MASS,
   readxl,
   naniar,
-  tidyverse
+  tidyverse,
+  Hmisc,
+  funModeling
 )
 
 
 # datos, arreglos y demas ####
 data = read_xlsx('data.xlsx') %>% as.data.table()
+names(data) = tolower(names(data))
+glimpse(data)
 
-head(data)
+# verificar nas
+naniar::vis_miss(data)
+nrow(data)
+data = data[!is.na(c_calidcocina)]
+nrow(data)
+
+# visualizacion de ceros
+print(status(data))
+
+# frecuencias de categorias
+freq(data)
+
+# descriptivas de la variables numericas
+print(profiling_num(data))
+plot_num(data)
+
+describe(data)
+
+# modificacion de variables y asignacion 
+
+data[, f_fechaavaluo := as.Date(f_fechaavaluo, '%Y/%m/%d')]
+
+var_num =
+  c('ant_avaluo_trim',
+    "area_privada_valfinal",
+    "area_privada_area")
+
+#Variables numericas
+df[, (var_num) := lapply(.SD, as.numeric), .SDcols = var_num]
+no_factor <- c("f_fechaavaluo", var_num)
+
+#Variables factor
+factor_vars <- names(df)[!(names(df) %in% no_factor)]
+df[, (factor_vars) := lapply(.SD, factor), .SDcols = factor_vars]
+
 
 
 
