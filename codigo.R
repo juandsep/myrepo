052c4b4b65f32c8aab3c92b1d25a271c519ffb66
unknown <juandsep@gmail.com>
2020-11-11 19:53:56 -0500

    limpieza de variables e imputacion con mice

--- codigo.R
+++ codigo.R
@@ -33,20 +33,67 @@ pacman::p_load(
   naniar,
   tidyverse,
   Hmisc,
-  funModeling
+  funModeling,
+  mice
+  
 )
-
 
 # datos, arreglos y demas ####
 data = read_xlsx('data.xlsx') %>% as.data.table()
 names(data) = tolower(names(data))
 glimpse(data)
 
+# modificacion de variables y asignacion 
+data[, f_fechaavaluo := as.Date(f_fechaavaluo, '%Y/%m/%d')]
+head(data)
+
+# Asignar tipos de var
+var_num =
+  c('ant_avaluo_trim',
+    'tot_banos',
+    'n_deposito',
+    'n_habitaciones',
+    'n_totalgarajes',
+    'n_pisos',
+    'n_sotanos',
+    'n_vetustez',
+    'a_edipiso',
+    'k_ascensor',
+    'latitud_double',
+    'longitud_double',
+    "area_privada_valfinal",
+    'area_privada_area')
+
+#Variables numericas
+data[, (var_num) := lapply(.SD, as.numeric), .SDcols = var_num]
+no_factor = c("f_fechaavaluo", var_num)
+
+#Variables factor
+factor_vars = names(data)[!(names(data) %in% no_factor)]
+data[, (factor_vars) := lapply(.SD, factor), .SDcols = factor_vars]
+
+numericVars <- which(sapply(data, is.numeric)) 
+numericVarNames <- names(numericVars) 
+cat('Hay', length(numericVars), 'variables numericas')
+
+
 # verificar nas
 naniar::vis_miss(data)
 nrow(data)
 data = data[!is.na(c_calidcocina)]
 nrow(data)
+
+temp = data[, mget(var_num)]
+mean(temp$a_edipiso, na.rm = T) %>% print()
+
+# imputacion con mice
+# mice solo funciona con variables numericas
+mice(temp, m=5, maxit=50, meth='pmm',seed=500, printFlag = F) %>% 
+  mice::complete() -> temp
+
+data[, a_edipiso := temp$a_edipiso]
+
+naniar::vis_miss(data)
 
 # visualizacion de ceros
 print(status(data))
@@ -57,26 +104,7 @@ freq(data)
 # descriptivas de la variables numericas
 print(profiling_num(data))
 plot_num(data)
-
 describe(data)
-
-# modificacion de variables y asignacion 
-
-data[, f_fechaavaluo := as.Date(f_fechaavaluo, '%Y/%m/%d')]
-
-var_num =
-  c('ant_avaluo_trim',
-    "area_privada_valfinal",
-    "area_privada_area")
-
-#Variables numericas
-df[, (var_num) := lapply(.SD, as.numeric), .SDcols = var_num]
-no_factor <- c("f_fechaavaluo", var_num)
-
-#Variables factor
-factor_vars <- names(df)[!(names(df) %in% no_factor)]
-df[, (factor_vars) := lapply(.SD, factor), .SDcols = factor_vars]
-
 
 
 
